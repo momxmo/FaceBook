e3d401723f84bad51e8609d24bfca5d2084c7cfa
momxmo <876164651@qq.com>
2015-08-28 12:59:15 +0800

    第一个faceBoook v2.0版本（添加注释）

--- facebook_v2.0/src/main/java/mo/com/facebook_v20/MainActivity.java
+++ facebook_v2.0/src/main/java/mo/com/facebook_v20/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    //添加注测试
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
